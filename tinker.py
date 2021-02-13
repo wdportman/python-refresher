8f8a454d836ff616bdd0c50b441510ffd6897fc5
Will <wdportman@gmail.com>
2021-02-12 17:52:54 -0800

    Add tinker & test files

--- tinker.py
+++ tinker.py
@@ -1,2 +1,11 @@
-name = "Reiko"
-print f"She said her name is {name}."
+# try:
+    # Use "raise" to raise an error
+raise IndexError("This is an index error")
+# except IndexError as e:
+#     pass                 # Pass is just a no-op. Usually you would do recovery here.
+# except (TypeError, NameError):
+#     pass                 # Multiple exceptions can be handled together, if required.
+# else:                    # Optional clause to the try/except block. Must follow all except blocks
+#     print("All good!")   # Runs only if the code in try raises no exceptions
+# finally:                 #  Execute under all circumstances
+#     print("We can clean up resources here")
