50bc11c2631b563e98d67b9b022633355fbb43f2
Will <wdportman@gmail.com>
2021-02-12 18:15:58 -0800

    Tinker around w/ Python

--- tinker.py
+++ tinker.py
@@ -1,11 +1,3 @@
-# try:
-    # Use "raise" to raise an error
-raise IndexError("This is an index error")
-# except IndexError as e:
-#     pass                 # Pass is just a no-op. Usually you would do recovery here.
-# except (TypeError, NameError):
-#     pass                 # Multiple exceptions can be handled together, if required.
-# else:                    # Optional clause to the try/except block. Must follow all except blocks
-#     print("All good!")   # Runs only if the code in try raises no exceptions
-# finally:                 #  Execute under all circumstances
-#     print("We can clean up resources here")
+def keyword_args(**kwargs):
+    return kwargs
+print(keyword_args(big="foot", loch="ness"))  # => {"big": "foot", "loch": "ness"}
