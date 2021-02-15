941853f9dabb5ba6f183c0146a864b9e74328072
Will <wdportman@gmail.com>
2021-02-14 17:22:43 -0800

    Build first small Python app

--- 04_first_python_app/code.py
+++ 04_first_python_app/code.py
@@ -1,5 +1,3 @@
-user_age = input("Enter your age: ")
-age_number = int(user_age)
-
-months = age_number * 12
-print(f"{age_number} is equal to {months} months.")
+user_age = int(input("Enter your age: "))
+months = user_age * 12
+print(f"{user_age} is equal to {months} months.")
